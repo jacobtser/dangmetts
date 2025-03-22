a415b9d6dc83f78e7fd102a68cf8da6975a55307
Jacob Tser Doku <jacobtser20@gmail.com>
2025-03-22 20:18:56 +0000

    Final

--- app.py
+++ app.py
@@ -1749,4 +1749,4 @@ def activity_logs():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables if they don't exist
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=False)
